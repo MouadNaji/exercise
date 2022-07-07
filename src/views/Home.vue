fe6703106884e650115e143915d7f404230027b4
MouadNaji <moua0005@stud.kea.dk>
2022-07-07 15:25:30 +0200

    tailwind css and eslint working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld class="text-red-400 text-xl" msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
